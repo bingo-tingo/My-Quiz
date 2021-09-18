8b7bfd7b69731b6af9f31df7583ad716feed3b07
bingo-tingo <77322132+bingo-tingo@users.noreply.github.com>
2021-09-18 15:57:27 +0530

    Add files via upload

---
+++ js/Question.js
@@ -0,0 +1,64 @@
+class Question {
+
+  constructor() {
+    this.title = createElement('h1')
+    this.input1 = createInput("Enter Your Name Here....");
+    this.input2 = createInput("Enter Correct Option No..");
+    this.button = createButton('Submit');
+    this.button2 = createButton('Reset');
+    this.question = createElement('h3');
+    this.option1 = createElement('h4');
+    this.option2 = createElement('h4');
+    this.option3 = createElement('h4');
+    this.option4 = createElement('h4');
+  }
+
+  hide(){
+    this.title.hide();
+    this.input1.hide();
+    this.button.hide();
+    //this.button2.hide();
+    this.input2.hide();
+  }
+
+  display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(610, 0);
+
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.position(150, 80);
+    this.option1.html("1: Everyone " );
+    this.option1.position(250, 120);
+    this.option2.html("2: Envelope" );
+    this.option2.position(250, 150);
+    this.option3.html("3: Estimate " );
+    this.option3.position(250, 180);
+    this.option4.html("4: Example" );
+    this.option4.position(250, 210);
+
+    this.input1.position(440, 330);
+    this.input2.position(690, 330);
+    this.button.position(690, 400);
+    this.button2.position(600, 400);
+
+    this.button.mousePressed(()=>{
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      contestant.name = this.input1.value();
+      contestant.answer = this.input2.value();
+      contestantCount+=1;
+      contestant.index = contestantCount;
+      contestant.update();
+      contestant.updateCount(contestantCount);
+    });
+
+    this.button2.mousePressed(()=>{
+      contestant.updateCount(0)
+      quiz.update(0)
+      var ref = database.ref("contestants")
+      ref.remove()
+    })
+  }
+}
